b15518deadbbe4a0994bf4a2c90c94a70c2979c5
Husanboy <husanboydeveloper@gmail.com>
2022-09-13 20:21:15 +0500

    register

---
+++ src/components/Kursga yozilish/style.js
@@ -0,0 +1,93 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+width: 100%;
+height: 1000px;
+
+
+`
+export const Wrap = styled.div`
+text-align: center;
+margin-top: 100px;
+`
+export const Text = styled.div`
+font-style: normal;
+font-weight: 800;
+font-size: 45px;
+line-height: 54px;
+text-align: center;
+color: #FF9D42;
+ `
+ export const Color = styled.span`
+ font-style: normal;
+ font-weight: 800;
+ font-size: 45px;
+ line-height: 54px;
+ text-align: center;
+ color:#000000 ;
+ ` 
+ export const InputContainer =styled.div`
+ width: 600px;
+ height: 800px;
+ background-color: wheat;
+ margin: auto;
+ margin-top:27px;
+ background: #FFFFFF;
+border: 1px solid #EFF2F7;
+box-shadow: 5px 5px 30px rgba(0, 0, 0, 0.25);
+ 
+ `
+export const Input = styled.input`
+width: 500px;
+height: 65px;
+background: #F8FAFC;
+border: 2px solid #EFF2F7;
+border-radius: 6px;
+font-style: normal;
+font-weight: 400;
+font-size: 16px;
+line-height: 19px;
+padding: 24px;
+margin: 0.5px 55px;
+margin-top: 30px;
+color: #8894A0;
+`
+export  const Button =styled.button`
+width: 500px;
+height: 65px;
+background: #FF9D42;
+border-radius: 6px;
+font-style: normal;
+font-weight: 600;
+font-size: 18px;
+line-height: 24px;
+text-align: center;
+background: #FF9D42;
+border-radius: 6px;
+color: #FFFFFF;
+margin-left: 55px;
+margin-top: 85px;
+`
+
+
+export const Select = styled.select`
+width: 500px;
+height: 65px;
+background: #F8FAFC;
+border: 2px solid #EFF2F7;
+border-radius: 6px;
+font-style: normal;
+font-weight: 400;
+font-size: 16px;
+line-height: 19px;
+margin: 0.5px 55px;
+margin-top: 30px;
+margin-right: 10px;
+color: #8894A0;
+`
+export const Option = styled.option`
+width: 500px;
+height: 30px;
+background: rgba(247, 247, 247, 0.8);
+border-radius: 5px 5px 0px 0px;
+`
--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import Kursgayozil from '../../components/Kursga yozilish'
 
 export const Register = () => {
   return (
-    <div>Register</div>
+    <Kursgayozil/>
   )
 }
 
