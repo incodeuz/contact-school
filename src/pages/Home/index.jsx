77d17c5974b3d0272134b3c699abec5b40ac9be6
SiddikovMukhammadrasul <siddiqov0706@gmail.com>
2022-09-18 20:14:39 +0900

    main done

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Advantage from '../../components/Advantage/Advantage.jsx';
 import Connection from '../../components/Connection/index.jsx';
+import Opener from '../../components/Opener/Opener.jsx';
 import Owner from '../../components/Owner/Owner.jsx';
 import Question from '../../components/Question/index.jsx';
 
 export const Home = () => {
   return (
     <div>
+      <Opener/>
       <Advantage />
       <Owner />
       <Question />
